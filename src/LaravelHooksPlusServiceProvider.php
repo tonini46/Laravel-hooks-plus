dc9a8df75f3b129f71501a765fc3f48f05f8526d
tonini46 <tonini46@gmail.com>
2024-02-20 16:05:25 +0300

    initial

---
+++ src/LaravelHooksPlusServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tonini46\LaravelHooksPlus;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Tonini46\LaravelHooksPlus\Hooks\Hooks;
+
+class LaravelHooksServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        Blade::directive('doAction', function ($expression) {
+            $expressions = explode(',', $expression);
+            $name = $expressions[0];
+            $arg = $expressions[1] ?? "''";
+            return "<?php echo do_action($name, $arg); ?>";
+        });
+    }
+
+    /**
+     * Register all directives.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('hooks', function ($app) {
+            return new Hooks();
+        });
+    }
+}
